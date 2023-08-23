e8baad310bd084a9cf65dea0b91ceca5caba204a
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-08-23 13:22:48 +0300

    fix my code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-""" FizzBuzz
-"""
-import sys
-
-
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n according to FizzBuzz rules.
-
-    - For multiples of three print "Fizz" instead of the number.
-    - For multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    - Otherwise, print the number itself.
-    """
-    if n < 1:
-        return
-
     for i in range(1, n + 1):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz")
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
         elif i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end=' ')
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=' ')
         else:
-            print(i)
-
+            print(i, end=' ')
+    print()
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+    import sys
+    if len(sys.argv) != 2:
+        sys.exit("Usage: ./0-fizzbuzz.py <number>")
+    fizzbuzz(int(sys.argv[1]))
